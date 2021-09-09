a9c50cddbcd13d701e771b6f7d59f117a28f10d4
ethanarnold <emailtoethan@gmail.com>
2021-09-09 10:37:03 -0700

    did some string stuff and also loops stuff

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -2,22 +2,32 @@ public class MyMain {
     // Takes two Strings as input, and returns a String equal to the first half
     // of the shorter String plus the second half of the longer String.
     public static String halfAdder(String str1, String str2) {
-        // REPLACE THIS WITH YOUR CODE
-        return "";
+        if(str1.length()>str2.length()) {
+            return str2.substring(0,str2.length()/2)+str1.substring(str1.length()/2);
+        }
+        return str1.substring(0,str1.length()/2)+str2.substring(str2.length()/2);
     }
 
     // Takes a String as input, and returns true if the String contains an 'A' or 'a'
     // and returns false otherwise
     public static boolean checkIfA(String str) {
-        // REPLACE THIS WITH YOUR CODE
+        //return(str.contains('A') || str.contains('a'))
+
+        for(int i = 0; i<str.length()-1; i++) {
+            if(str.charAt(i)=='a' || str.charAt(i)=='A') return true;
+        }
         return false;
     }
 
     //  Takes in two Strings as input and returns a String equal to those two Strings added
     //  together in alphabetic order.
     public static String stringOrder(String str1, String str2) {
-        return "";
+        if(str2.compareToIgnoreCase(str1)>=0) {
+            return str1 + str2;
+        }
+        return str2+str1;
     }
+
 
 
     public static void main(String[] args) {
@@ -42,5 +52,6 @@ public class MyMain {
         System.out.println(stringOrder("cranberry", "banana")); // bananacranberry
         System.out.println(stringOrder("apple", "Banana")); // appleBanana
         System.out.println(stringOrder("Cranberry", "apple")); // appleCranberry
+        System.out.println(stringOrder("Cranberry", "Craple"));
     }
 }
